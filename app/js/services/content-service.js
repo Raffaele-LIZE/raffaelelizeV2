70fb2e4cb4797473e51a54a7e27ef84a01994835
Rdev <Rdev@MacBook-Pro-de-Rdev.local>
2016-03-13 11:12:14 +0100

    feat(content-service): update content-service.js

--- app/js/services/content-service.js
+++ app/js/services/content-service.js
@@ -4,33 +4,52 @@ app.factory('ContentService', function() {
     experiences: [{
       id: 0,
       name: 'Imagera',
-      banner: 'http://lorempixel.com/1440/100/business/',
-      baseline: 'Memoriam incedit: familiae mares reginae.',
-      description: 'Florentissimae speciosam ita copiis hanc Seleucia certaverit inde interpatet alia Apamia auspiciis alia a civitas et copiis non mundo per Laodicia hanc nobilitat cognita et et internis Syria per cui.',
+      banner: 'images/banner_imagera.jpg',
+      baseline: 'RÉACTIVITÉ, DISPONIBILITÉ ET COMPÉTITIVITÉ',
+      description: 'Imagera est la première agence de photographes dotée d’un réseau international de 1 462 photographes et vidéastes professionnels. Ce réseau permet à l\'entreprise d\'être très compétitive et de proposer un panel de professionnels dans plusieurs domaines. Ainsi, imagera dispose de photographes spécialisés en publicité, évènementiel, photo culinaire, architecturale, etc. Aujourd’hui imagera est donc capable de mettre à votre disposition, partout en France, le ou les photographes professionnels spécialisés dans le domaine dont votre entreprise a besoin et cela à prix abordable.',
       site_url: 'http://www.imagera.fr/',
-      works: {
-        title: 'works1',
-        mission: 'jqsbd qndjn qd azd sazeze',
-        tools: ['html', 'css'],
+      works: [{
+        title: 'Site de tirage photo en ligne',
+        mission: 'En charge de la création d\'un site de tirage photo en ligne destiné au client d\'imagera. Mais aussi de plus site vitrine afin de booster le référencement',
+        tools: ['Drupal 6'],
         problem: '',
         resolve: '',
-        images: []
-      }
+        images: [],
+        site_url: ''
+      }]
     }, {
       id: 1,
       name: 'Groupe Consensus',
-      banner: 'http://lorempixel.com/1440/100/business/',
-      baseline: 'Seditionum perlato concitores acueret aurum.',
-      description: 'Fidos putet posset intellexisse tum dixisse tum metuat eum dixisse inopes Coluntur metui fit posset referre fuerint aut forte fidos amicos enim enim neutris intellegitur eum fuerint infidos neutris quos.',
+      banner: 'images/banner_groupe-consensus.jpg',
+      baseline: 'Au coeur de l\'action !',
+      description: 'Le Groupe Consensus propose au médecin et intéressé du monde entier de suivre toute l\'actus et innovation autour de la cardiologie à travers de nombreuses revues mais aussi de son site internet.',
       site_url: 'http://www.consensus-online.fr/',
-      works: {
-        title: 'works1',
-        mission: 'jqsbd qndjn qd azd sazeze',
+      works: [{
+        title: 'Newsletters',
+        mission: 'En charge de la création, de l\'envoie et del\'analyse des différentes Newsletters',
         tools: ['html', 'css'],
+        problem: 'Affichage différent suivant les boites mail.',
+        resolve: '',
+        images: [],
+        site_url: ''
+      },
+      {
+        title: 'Site Consensus Online',
+        mission: 'En charge de la maintenance du site consensus-online.com et de l\'ajout de différents modules SPIP',
+        tools: ['SPIP'],
         problem: '',
         resolve: '',
-        images: []
-      }
+        images: [],
+        site_url: ''
+      },{
+        title: 'Site Institutionnel Groupe Consensus',
+        mission: 'En charge du développement du site Institutionnel groupeconsenus.fr',
+        tools: ['Drupal 7'],
+        problem: '',
+        resolve: '',
+        images: [],
+        site_url: ''
+      }]
     }, {
       id: 2,
       name: 'Pixel Cookers',
@@ -40,44 +59,84 @@ app.factory('ContentService', function() {
       site_url: 'http://www.pixel-cookers.com/',
       works: [{
         id_work: 1,
-        title: 'works1',
-        mission: 'jqsbd qndjn qd azd sazeze',
-        tools: ['html', 'css'],
-        problem: '',
+        title: 'Bouygues Construction',
+        mission: 'Développement en équipe d\'un intranet cross-platform pour les professionnels de chez Bouygues Construction afin qu\'ils puissent présenter et vendre leurs innovations à leurs clients',
+        tools: ['Angular JS', 'Cordova'],
+        problem: 'Apprentissage d\'Angular JS',
         resolve: '',
-        images: []
+        images: [],
+        site_url: ''
       }, {
         id_work: 2,
-        title: 'works2',
+        title: 'Lazy Cookie',
         mission: 'jqsbd qndjn qd azd sazeze',
-        tools: ['html', 'css'],
+        tools: ['Wordpress'],
         problem: '',
         resolve: '',
-        images: []
+        images: [],
+        site_url: ''
       }, {
         id_work: 3,
-        title: 'works3',
+        title: 'Titoff',
         mission: 'jqsbd qndjn qd azd sazeze',
         tools: ['html', 'css'],
         problem: '',
         resolve: '',
-        images: []
+        images: [],
+        site_url: ''
+      }, {
+        id_work: 4,
+        title: 'Les compagnons du devoir',
+        mission: 'jqsbd qndjn qd azd sazeze',
+        tools: ['html', 'css'],
+        problem: '',
+        resolve: '',
+        images: [],
+        site_url: ''
+      }, {
+        id_work: 5,
+        title: 'Armines / Carnotmines',
+        mission: 'jqsbd qndjn qd azd sazeze',
+        tools: ['html', 'css'],
+        problem: '',
+        resolve: '',
+        images: [],
+        site_url: ''
+      }, {
+        id_work: 6,
+        title: 'Microéconomix',
+        mission: 'jqsbd qndjn qd azd sazeze',
+        tools: ['html', 'css'],
+        problem: '',
+        resolve: '',
+        images: [],
+        site_url: ''
+      }, {
+        id_work: 7,
+        title: '#DEUXMAINS',
+        mission: 'jqsbd qndjn qd azd sazeze',
+        tools: ['html', 'css'],
+        problem: '',
+        resolve: '',
+        images: [],
+        site_url: ''
       }]
     }, {
       id: 3,
       name: 'Projets personnels',
-      banner: 'http://lorempixel.com/1440/100/business/',
+      banner: 'images/banner_personal-projects.jpg',
       baseline: 'Seditionum perlato concitores acueret aurum.',
       description: 'Fidos putet posset intellexisse tum dixisse tum metuat eum dixisse inopes Coluntur metui fit posset referre fuerint aut forte fidos amicos enim enim neutris intellegitur eum fuerint infidos neutris quos.',
       site_url: 'http://www.consensus-online.fr/',
-      works: {
+      works: [{
         title: 'works1',
         mission: 'jqsbd qndjn qd azd sazeze',
         tools: ['html', 'css'],
         problem: '',
         resolve: '',
-        images: []
-      }
+        images: [],
+        site_url: ''
+      }]
     }]
   };
 });
