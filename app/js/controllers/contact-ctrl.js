bc62d0c0d70f339d6eeb756cfe3b26ff5025a508
Rdev <Rdev@MacBook-Pro-de-Rdev.local>
2016-03-13 12:50:42 +0100

    feat(contact): add send mail function

--- app/js/controllers/contact-ctrl.js
+++ app/js/controllers/contact-ctrl.js
@@ -1,3 +1,16 @@
 app.controller('ContactCtrl', function($scope) {
 
+  $scope.sendMail = function() {
+    console.log('sendMail');
+    var lastName = document.querySelector('#lastname').value,
+        firstName = document.querySelector('#firstname').value,
+        mail = document.querySelector('#mail').value,
+        comment = document.querySelector('#comment').value;
+
+    var link = 'mailto:raffaele.rdev@gmail.com' +
+                '?subject=Contact from raffaelelize.fr' +
+                '&body=' + lastName + ' ' + firstName + ' \n ' + mail +  ' \n ' + comment;
+
+    window.location.href = link;
+  }
 });
